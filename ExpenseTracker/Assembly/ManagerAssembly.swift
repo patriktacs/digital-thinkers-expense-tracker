9eaa94d4d8733160218f90676f032871c3f3168a
Ács Patrik Tamás <patriktacs.reg@protonmail.com>
2022-04-15 16:36:35 +0200

    feature - remove initial ViewController, set up dependency injection

---
+++ ExpenseTracker/Assembly/ManagerAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  ManagerAssembly.swift
+//  ExpenseTracker
+//
+//  Created by Ács, Patrik, Vodafone Hungary on 2022. 04. 15..
+//
+
+import Foundation
---
+++ ExpenseTracker/Assembly/ViewAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  ViewAssembly.swift
+//  ExpenseTracker
+//
+//  Created by Ács, Patrik, Vodafone Hungary on 2022. 04. 15..
+//
+
+import Foundation
